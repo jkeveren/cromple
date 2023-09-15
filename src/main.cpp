9a5bdbde1b73be6b8c0456e0ec8eef5b01d65e86
James Keveren <james@keve.ren>
2023-09-15 02:40:20 +0100

    Big error handling thing!

--- src/main.cpp
+++ src/main.cpp
@@ -10,19 +10,51 @@
 // Access all global names with "::<name>" e.g. "::FILE" or "::error_t".
 namespace program_n {
 
+	// Use std::expect<`return-type`, error_t> as return value for all functions that can error in order to remove the nead to try-catch every errorable function call just to add more error information at each level.
+	// The goal is for a deep error caused by a high level mistake to be easy to debug just by seeing the error message. All relavent variable values should be in the error messages.
+	// However deep errors should not be possible to branch on. Only the highest level error. This allows the implementation of the unit to be changed without fear of breaking changes.
+	// This error handling strategy has overhead so might want to be neutered for performance-crittical applications that expect to error a lot. Perhaps by making .append() do nothing (this would maintain .reason functionality).
 	class error_t {
+		private:
+		// message_stack is a string of error messages seperated by newlines.
+		// Each message is appended in chronological order. This results in the lowest level messages being at the top of the stack and are the first ones printed.
+		// Each message should be verbose and contain lots of information that could be useful in debugging the issue like labeled variable values.
+		// message_stack is private because deep errors should not be machine readable.
+		// Machine readable errors become part of a units external API.
+		// This causes low level changes to the unit to effect the external API which makes them breaking changes.
+		// If a user of a unit need low level error access then the user should implement it or the unit should provide a way of injecting low level resources.
+		std::string message_stack;
+
 		public:
+		// Common reasons are define here.
+		// When defining your own reasons, make sure to start the enum at custom_reason_start!
+		// Using a regular enum for implicit conversion to int.
+		enum reasons {
+			reason_none__do_not_use_this_reason_on_purpose = 0, // A reason of 0 could be interpreted accidentally as no reason so let's make that official so there are less mistakes. This should not be used on puropse.
+			reason_other = 1, // Other should be used when an error occured but you don't want to make the machine readable reason part of the external API. For implementation details that are likely to change.
+			// none__... and other being 0 and 1 makes them also work as sensible exit codes so you can just exit with the error reason which defaults to other which is 1.
+			custom_reason_start, // Start custom reason enums at this int.
+		};
 
-		// int error = 0;
-		// TODO: implement machine readable error identifiers.
-		// Make errors stackable so very specifc errors can be caught.
-		std::string message_stack = "";
+		// reason is a machine readable version of the latest error appended to message_stack.
+		// This does not stack like message_stack for the same reasons message_stack is private.
+		// It only stores the highest level reason for the error.
+		// default to other. If an error occurred, there was a reason but we can.t assume what it is here.
+		int reason = reason_other;
+		
+		error_t(std::string message, int reason = reason_other) : message_stack{message},  reason{reason} {}
 
-		error_t(std::string message) : message_stack{message} {}
-
-		error_t append(std::string message) {
+		error_t append(std::string message, int reason = reason_other) {
+			this->reason = reason;
 			message_stack += message + "\n";
 			return *this;
+		}
+
+		// Prints the message_stack and returns reason int (nice for exiting program).
+		// message_stack is only accessible through printing to prevent easy access to the user unit for the reasons stated above in the section about message_stack etc.
+		int print() {
+			std::cerr << message_stack << std::flush;
+			return reason;
 		}
 	};
 
@@ -32,21 +64,23 @@ namespace program_n {
 		::FILE *pipe = NULL;
 
 		public:
-		// enum class error {
-		// 	no_error = 0,
-		// 	command_already_running,
-		// };
+		enum error_reasons {
+			error_reason_command_already_running = error_t::custom_reason_start,
+		};
 
 		std::string command_string;
 		const char *type; // passed to popen.
 
 		command_t(std::string command_string, const char *type) : command_string{command_string}, type{type} {}
 
-		// Starts process and returns nothing. This does not wait for the process to exit.
+		// Starts process and returns pipe. This does not wait for the process to exit.
 		std::expected<::FILE *, error_t>
 		start() {
 			if (running) {
-				return std::unexpected(error_t(std::format("Command is already running: \"{}\".", command_string)));
+				return std::unexpected(error_t(
+					std::format("Command is already running: \"{}\".", command_string),
+					error_reason_command_already_running
+				));
 			}
 
 			running = true;
@@ -73,7 +107,9 @@ namespace program_n {
 
 			// If terminated by a signal.
 			if (WIFEXITED(code) == 0) {
-				throw std::runtime_error(std::format("Compilation command exited abnormally: {}", std::strerror(errno)));
+				// Should return throw? Is it an error.
+				// Inside a generic command runner class this should probably just be returned.
+				return std::unexpected(error_t(std::format("Compilation command exited abnormally: {}", std::strerror(errno))));
 			}
 
 			// Get the actual status code.
@@ -84,13 +120,13 @@ namespace program_n {
 
 		// Runs the command to completion and returns status code.
 		std::expected<int, error_t> run() {
-			std::expected<FILE *, error_t> pipe = start();
+			std::expected<::FILE *, error_t> pipe = start();
 			if (!pipe) {
-				return std::unexpected(pipe.error().append("Error starting command."));
+				return std::unexpected(pipe.error().append("Error starting command.", pipe.error().reason));
 			}
 			std::expected<int, error_t> status = wait();
 			if (!status) {
-				return std::unexpected(status.error().append("Error wating for command to complete."));
+				return std::unexpected(status.error().append("Error wating for command to complete.", pipe.error().reason));
 			}
 			return status;
 		}
@@ -113,16 +149,16 @@ namespace program_n {
 		}
 
 		if (origin_file_path.empty()) {
-			std::cerr << "Origin file was not provided." << std::endl;
-			return 1;
+			return error_t("Origin file was not provided.").print();
 		}
 
 		// Compile file.
-		command_t command(std::string("g++ ") + origin_file_path.string(), "r");
-		std::expected<int, error_t> status = command.run();
+		std::expected<int, error_t> status = command_t(std::string("g++ ") + origin_file_path.string(), "r").run();
 		if (!status) {
-			std::cerr << status.error().message_stack << "Error running compilation command." << std::endl;
-			return 1;
+			if (status.error().reason == command_t::error_reason_command_already_running) {
+				std::cout << "ur dumb" << std::endl;
+			}
+			return status.error().append("Error running compilation command.").print();
 		}
 
 		std::cout << "Compiler exited with status: " << status.value() << std::endl;
