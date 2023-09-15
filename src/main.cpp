67ba9e3456ccc45bb3a3d84916b2e481fc6f3f26
James Keveren <james@keve.ren>
2023-09-15 03:00:07 +0100

    cleanup and very cool meme referencing

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,12 @@
 // Access all global names with "::<name>" e.g. "::FILE" or "::error_t".
 namespace program_n {
 
+	// Error handling strategy.
 	// Use std::expect<`return-type`, error_t> as return value for all functions that can error in order to remove the nead to try-catch every errorable function call just to add more error information at each level.
 	// The goal is for a deep error caused by a high level mistake to be easy to debug just by seeing the error message. All relavent variable values should be in the error messages.
 	// However deep errors should not be possible to branch on. Only the highest level error. This allows the implementation of the unit to be changed without fear of breaking changes.
 	// This error handling strategy has overhead so might want to be neutered for performance-crittical applications that expect to error a lot. Perhaps by making .append() do nothing (this would maintain .reason functionality).
+	// There's more comments here than actual code.
 	class error_t {
 		private:
 		// message_stack is a string of error messages seperated by newlines.
@@ -22,38 +24,41 @@ namespace program_n {
 		// message_stack is private because deep errors should not be machine readable.
 		// Machine readable errors become part of a units external API.
 		// This causes low level changes to the unit to effect the external API which makes them breaking changes.
-		// If a user of a unit need low level error access then the user should implement it or the unit should provide a way of injecting low level resources.
+		// If a user of a unit needs low level error access then the user should implement it and/or the unit should provide a way of injecting low level resources.
 		std::string message_stack;
 
 		public:
+		// reason is a machine readable version of the latest error appended to message_stack.
+		// This does not stack like message_stack for the same reasons message_stack is private.
+		// It only stores the highest level reason for the error.
+		// Defaults to other. If an error occurred, there was a reason but we can't assume what it is here.
+		int reason = reason_other;
+
 		// Common reasons are define here.
 		// When defining your own reasons, make sure to start the enum at custom_reason_start!
 		// Using a regular enum for implicit conversion to int.
 		enum reasons {
 			reason_none__do_not_use_this_reason_on_purpose = 0, // A reason of 0 could be interpreted accidentally as no reason so let's make that official so there are less mistakes. This should not be used on puropse.
-			reason_other = 1, // Other should be used when an error occured but you don't want to make the machine readable reason part of the external API. For implementation details that are likely to change.
-			// none__... and other being 0 and 1 makes them also work as sensible exit codes so you can just exit with the error reason which defaults to other which is 1.
-			custom_reason_start, // Start custom reason enums at this int.
+			reason_other = 1, // Other should be used when an error occured but you don't want to make the machine readable reason part of the external API. Mostly for implementation details that can change.
+			// reason_none and reason_other being 0 and 1 makes them also work as sensible exit codes so you can just exit with the error reason which defaults to other which is 1. Not a great strat if you want unique exit codes for different circumstances.
+			custom_reason_start, // Custom reasons should start at this int to avoid colliding with the previous reasons.
 		};
-
-		// reason is a machine readable version of the latest error appended to message_stack.
-		// This does not stack like message_stack for the same reasons message_stack is private.
-		// It only stores the highest level reason for the error.
-		// default to other. If an error occurred, there was a reason but we can.t assume what it is here.
-		int reason = reason_other;
 		
-		error_t(std::string message, int reason = reason_other) : message_stack{message},  reason{reason} {}
+		// Constructor
+		error_t(const std::string &message, int reason = reason_other) : message_stack{message}, reason{reason} {}
 
-		error_t append(std::string message, int reason = reason_other) {
+		// Appends the highest level error message to the message stack and overrides the reason.
+		// Returns self for chaining.
+		error_t append(const std::string &message, int reason = reason_other) {
 			this->reason = reason;
 			message_stack += message + "\n";
 			return *this;
 		}
 
-		// Prints the message_stack and returns reason int (nice for exiting program).
+		// Prints the message_stack and returns reason int (handy for exiting program e.g. "int main() {...; return something.error().append("OOPSIE WOOPSIE!! Uwu We made a fucky wucky!!").print();}").
 		// message_stack is only accessible through printing to prevent easy access to the user unit for the reasons stated above in the section about message_stack etc.
-		int print() {
-			std::cerr << message_stack << std::flush;
+		int print(std::ostream &output_stream = std::cerr) {
+			output_stream << message_stack << std::flush;
 			return reason;
 		}
 	};
